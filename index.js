e9299a84a69f2333cea96f6c7c289902301bf654
Ridha Akmal <ridhaakmalputra@gmail.com>
2022-08-30 21:50:12 +0700

    Add Auth route, Auth controller, and Auth middleware

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import dotenv from "dotenv";
+import SequelizeStore from "connect-session-sequelize";
 import UserRoute from "./routes/UserRoute.js";
 import ProductRoute from "./routes/ProductRoute.js";
-/*import db from "./config/Database.js"
-(async()=>{
+import AuthRoute from "./routes/AuthRoute.js";
+import db from "./config/Database.js"
+/*(async()=>{
     await db.sync();
 })();*/
 dotenv.config();
 
 const app = express();
+const sessionStore = SequelizeStore(session.Store);
+const store = new sessionStore({
+    db: db
+});
+//store.sync();
 
 app.use(session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
+    store: store,
     cookie:{
         secure:'auto'
     }
@@ -28,6 +36,7 @@ app.use(cors({
 app.use(express.json());
 app.use(UserRoute);
 app.use(ProductRoute);
+app.use(AuthRoute);
 
 app.listen(process.env.APP_PORT, ()=>{
     console.log('Server up and running...');
